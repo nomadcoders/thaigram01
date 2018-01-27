fb141b83bcf6dad9f4de03f95a871b00168a4c42
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-01-27 17:34:45 +0700

    Likes, Comments and __str__

--- images/admin.py
+++ images/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Image
+from .models import Image, Comment, Like
 # Register your models here.
 admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Like)
--- images/models.py
+++ images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Image(models.Model):
@@ -6,5 +7,25 @@ class Image(models.Model):
     file = models.FileField()
     location = models.CharField(max_length=80)
     caption = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.location} - {self.caption}'
+
+
+class Comment(models.Model):
+
+    message = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    created_for = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    created_for = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
