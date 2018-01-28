c2f87ebe90ca99f4e87dccc4e8db56e24dba70f2
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-01-28 12:05:12 +0700

    Views

--- images/views.py
+++ images/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    user = request.user
+    if user.is_authenticated:
+        return HttpResponse('You have reached the home!')
+    else:
+        return HttpResponse('You have to log in')
